324af117fc983a25dd541856873ff340146e6290
khanhtlu2020 <99171141+khanhtlu2020@users.noreply.github.com>
2024-09-30 03:13:03 +0700

    Ex05_2: Added user interactions and event handlers

--- Ex05_2/app/src/main/java/com/example/ex05_2/MainActivity.java
+++ Ex05_2/app/src/main/java/com/example/ex05_2/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.ex05_2;
 
 import android.os.Bundle;
+import android.app.Activity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.View;
+import java.text.DecimalFormat;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +14,81 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity {
+    Button btnTieptuc,btnGiai,btnThoat;
+    EditText edita,editb,editc;
+    TextView txtkq;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.TableLayout2), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        btnTieptuc= findViewById(R.id.btntieptuc);
+        btnGiai= findViewById(R.id.btngiaipt);
+        btnThoat=findViewById(R.id.btnthoat);
+        edita= findViewById(R.id.edta);
+        editb= findViewById(R.id.edtb);
+        editc= findViewById(R.id.edtc);
+        txtkq= findViewById(R.id.txtkq);
+        btnGiai.setOnClickListener(new  View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                String sa=edita.getText()+"";
+                String sb=editb.getText()+"";
+                String sc=editc.getText()+"";
+                int a=Integer.parseInt(sa);
+                int b=Integer.parseInt(sb);
+                int c=Integer.parseInt(sc);
+                String kq="";
+                DecimalFormat dcf=new DecimalFormat("0.00");
+                if(a==0)
+                {
+                    if(b==0) {
+                        if(c==0)
+                            kq="PT vô số nghiệm";
+                        else
+                            kq="PT vô nghiệm";
+                    }
+                    else
+                    {
+                        kq="Pt có 1 No, x="+dcf.format(-c/b);
+                    }
+                }
+                else
+                {
+                    double delta=b*b-4*a*c;
+                    if(delta<0)
+                    {
+                        kq="PT vô nghiệm";
+                    }
+                    else if(delta==0)
+                    {
+                        kq="Pt có No kép x1=x2="+dcf.format(-b/(2*a));
+                    }
+                    else
+                    {
+                        kq="Pt có 2 No: x1="+dcf.format((-b+Math.sqrt(delta))/(2*a))+"; x2="+dcf.format((- b-Math.sqrt(delta))/(2*a));
+                    }
+                }
+                txtkq.setText(kq);
+            }
+        });
+        btnTieptuc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                edita.setText("");
+                editb.setText("");
+                editc.setText("");
+                edita.requestFocus();
+            }
+        });
+        btnThoat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                finish();
+            }
         });
     }
 }
