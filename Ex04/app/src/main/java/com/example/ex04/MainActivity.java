5f93a5463b05db36abd3d7f161d53d471a418900
khanhtlu2020 <99171141+khanhtlu2020@users.noreply.github.com>
2024-09-29 22:24:17 +0700

    Ex04: Added user interactions and event handlers

--- Ex04/app/src/main/java/com/example/ex04/MainActivity.java
+++ Ex04/app/src/main/java/com/example/ex04/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.ex04;
 
 import android.os.Bundle;
+import android.app.Activity;
+import android.widget.EditText;
+import android.widget.Button;
+import android.view.View;
+import java.text.DecimalFormat;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +13,36 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity {
+EditText edtdoC,edtdoF;
+Button btncf,btnfc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        edtdoC = findViewById(R.id.txtCel);
+        edtdoF = findViewById(R.id.txtFar);
+        btncf = findViewById(R.id.btnFar);
+        btnfc = findViewById(R.id.btnCel);
+        btncf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                DecimalFormat dcf=new DecimalFormat("#.00");
+                String doC = edtdoC.getText()+"";
+                int C=Integer.parseInt(doC);
+                edtdoF.setText(""+dcf.format(C*1.8+32));
+            }
+        });
+        btnfc.setOnClickListener(new  View.OnClickListener()  {
+            @Override
+            public void onClick(View v) {
+                DecimalFormat dcf=new DecimalFormat("#.00");
+                // TODO Auto-generated method stub
+                String doF = edtdoF.getText()+"";
+                int F=Integer.parseInt(doF);
+                edtdoC.setText(""+dcf.format((F-32)/1.8));
+            }
         });
     }
 }
