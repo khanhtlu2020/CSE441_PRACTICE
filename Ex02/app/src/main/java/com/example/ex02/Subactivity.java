8549e4063e8f57571966a175f47c76fe4de5d240
khanhtlu2020 <99171141+khanhtlu2020@users.noreply.github.com>
2024-09-29 16:36:54 +0700

    Ex02 initial commit

---
+++ Ex02/app/src/main/java/com/example/ex02/Subactivity.java
@@ -0,0 +1,28 @@
+package com.example.ex02;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Subactivity extends AppCompatActivity {
+Button btnok;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subactivity);
+        btnok = findViewById(R.id.btnok);
+        btnok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub finish();
+                finish();
+            }
+        });
+    }
+}
