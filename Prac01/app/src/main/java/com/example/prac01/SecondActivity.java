b9d0069015c5a7207bde789fbbfc2958d713c444
khanhtlu2020 <99171141+khanhtlu2020@users.noreply.github.com>
2024-10-04 16:16:15 +0700

    Added user interactions and event handlers for second activity

--- Prac01/app/src/main/java/com/example/prac01/SecondActivity.java
+++ Prac01/app/src/main/java/com/example/prac01/SecondActivity.java
@@ -1,6 +1,10 @@
 package com.example.prac01;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,29 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class SecondActivity extends AppCompatActivity {
 
+    private EditText nameEditText, gpaEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_second);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        nameEditText = findViewById(R.id.nameEditText);
+        gpaEditText = findViewById(R.id.gpaEditText);
+        Button submitButton = findViewById(R.id.submitButton);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = nameEditText.getText().toString();
+                String gpa = gpaEditText.getText().toString();
+
+                Intent resultIntent = new Intent();
+                resultIntent.putExtra("name", name);
+                resultIntent.putExtra("gpa", gpa);
+                setResult(RESULT_OK, resultIntent);
+                finish();
+            }
         });
     }
 }
