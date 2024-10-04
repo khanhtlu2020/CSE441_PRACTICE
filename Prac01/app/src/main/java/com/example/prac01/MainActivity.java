7f920a4242a6e649ed2afc3dc29536099b08bfcb
khanhtlu2020 <99171141+khanhtlu2020@users.noreply.github.com>
2024-10-04 16:07:22 +0700

    Added user interactions and event handlers for main activity

--- Prac01/app/src/main/java/com/example/prac01/MainActivity.java
+++ Prac01/app/src/main/java/com/example/prac01/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.prac01;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,40 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE = 1;
+    private TextView resultTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button startActivityButton = findViewById(R.id.startActivityButton);
+        resultTextView = findViewById(R.id.resultTextView);
+
+        startActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivityForResult(intent, REQUEST_CODE);
+            }
         });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+            String name = data.getStringExtra("name");
+            String gpa = data.getStringExtra("gpa");
+
+            resultTextView.setText("Họ và tên: " + name + "\nĐiểm GPA: " + gpa);
+        }
+    }
 }
+
+
