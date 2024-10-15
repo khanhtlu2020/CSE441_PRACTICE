db3a0caef624e9bc8dbca76ea6a54c989ea7beb4
khanhtlu2020 <99171141+khanhtlu2020@users.noreply.github.com>
2024-10-16 05:00:14 +0700

    Final: Added user interactions and event handlers for child activity

--- Ex07_1/app/src/main/java/com/example/ex07_1/ChildActivity.java
+++ Ex07_1/app/src/main/java/com/example/ex07_1/ChildActivity.java
@@ -1,6 +1,11 @@
 package com.example.ex07_1;
 
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +14,21 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ChildActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+    // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_child);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        Button btn = (Button) findViewById(R.id.button1);
+        TextView txt1 = (TextView) findViewById(R.id.textView1);
+        btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+    // TODO Auto-generated method stub Intent intent1 = new
+                Intent intent1 = new Intent(ChildActivity.this,MainActivity.class);
+                startActivity(intent1);
+            }
         });
     }
 }
