9722ffd5e87da9fd47b5f8a717311e36a43e5b21
khanhtlu2020 <99171141+khanhtlu2020@users.noreply.github.com>
2024-10-16 04:51:30 +0700

    Added user interactions and event handlers for main activity

--- Ex07_1/app/src/main/java/com/example/ex07_1/MainActivity.java
+++ Ex07_1/app/src/main/java/com/example/ex07_1/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.ex07_1;
 
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,18 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        Button btn1 =(Button) findViewById(R.id.button);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+// TODO Auto-generated method stub Intent intent1 = new
+                Intent intent1 = new Intent( MainActivity.this, ChildActivity.class);
+                startActivity(intent1);
+            }
         });
     }
 }
