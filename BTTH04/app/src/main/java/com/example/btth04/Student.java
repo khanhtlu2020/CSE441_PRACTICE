fe98a726d99f808fb25d7732876e2ddeaa0118d4
khanhtlu2020 <99171141+khanhtlu2020@users.noreply.github.com>
2024-10-19 04:06:46 +0700

    Added Student class and XML Layouts

---
+++ BTTH04/app/src/main/java/com/example/btth04/Student.java
@@ -0,0 +1,49 @@
+package com.example.btth04;
+
+public class Student {
+    String key, ten, mssv, lop, diemtb;
+
+    public Student() {
+
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public String getMssv() {
+        return mssv;
+    }
+
+    public String getLop() {
+        return lop;
+    }
+
+    public String getDiemtb() {
+        return diemtb;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public void setMssv(String mssv) {
+        this.mssv = mssv;
+    }
+
+    public void setLop(String lop) {
+        this.lop = lop;
+    }
+
+    public void setDiemtb(String diemtb) {
+        this.diemtb = diemtb;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+}
