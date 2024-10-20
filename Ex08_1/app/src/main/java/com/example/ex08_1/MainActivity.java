38bb74147af5460481f642653549fda6d0db9a30
khanhtlu2020 <99171141+khanhtlu2020@users.noreply.github.com>
2024-10-20 15:30:11 +0700

    Final: Added user interactions and event handlers

--- Ex08_1/app/src/main/java/com/example/ex08_1/MainActivity.java
+++ Ex08_1/app/src/main/java/com/example/ex08_1/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.ex08_1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,31 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import kotlinx.coroutines.channels.Send;
 
+public class MainActivity extends AppCompatActivity {
+    Button btncallphone, btnsendsms;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        btncallphone = findViewById(R.id.btncallphone);
+        btnsendsms = findViewById(R.id.btnsendsms);
+        //Xử lý click
+        btncallphone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Khai báo Intent công khai
+                Intent myintent = new Intent(MainActivity.this, CallPhoneActivity.class);
+                startActivity(myintent);
+            }
+        });
+        btnsendsms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myintent2 = new Intent(MainActivity.this, SendSMSActivity.class);
+                startActivity(myintent2);
+            }
         });
     }
 }
--- Ex08_1/app/src/main/java/com/example/ex08_1/SendSMSActivity.java
+++ Ex08_1/app/src/main/java/com/example/ex08_1/SendSMSActivity.java
@@ -1,6 +1,12 @@
 package com.example.ex08_1;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.EditText;
+import android.net.Uri;
+import android.content.Intent;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +14,29 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class SendSMSActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_send_smsactivity);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+    public class SendSMSActivity extends AppCompatActivity {
+        EditText edtsms;
+        Button btnback2;
+        ImageButton btnsendsms;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_send_smsactivity);
+            edtsms = (EditText) findViewById(R.id.edtsms);
+            btnback2 = (Button) findViewById(R.id.btnback2);
+            btnsendsms = (ImageButton) findViewById(R.id.btnsendsms);
+            btnsendsms.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent callintent = new Intent(Intent.ACTION_SENDTO, Uri.parse("smsto:"+edtsms.getText().toString()));
+                    startActivity(callintent);
+                }
+            });
+            btnback2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    finish();
+                }
+            });
+        }
     }
-}
