3b94749cb4d251abe6383736616d969de563e9f4
khanhtlu2020 <99171141+khanhtlu2020@users.noreply.github.com>
2024-10-21 01:44:26 +0700

    Final: MainActivity class

--- Ex20/app/src/main/java/com/example/ex20/MainActivity.java
+++ Ex20/app/src/main/java/com/example/ex20/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.ex20;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +11,31 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnStart;
+    // Khai báo MyAsyncTask
+    MyAsyncTask mytt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btnStart = (Button) findViewById(R.id.button1);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+                doStart();
+            }
         });
+    }
+
+    private void doStart() {
+        // Truyền this (chính là MainActivity hiện tại) qua background Thread
+        mytt = new MyAsyncTask(this);
+
+        // Kích hoạt tiến trình
+        // Khi gọi hàm này thì onPreExecute của mytt sẽ thực thi trước
+        mytt.execute();
     }
 }
